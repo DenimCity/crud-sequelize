7b8844f1e54552c9c07ddd42d5264e8e0c83fd89
DenimCity <altidorj1@gmail.com>
2020-04-02 00:37:54 -0400

    added tests

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,7 +1,15 @@
 const request = require('supertest');
 const app = require('../server');
+const routers = require('../routes')
 
 describe('Post Endpoints', () => {
+
+    describe('Routes', () => {
+        it('should have 6 routes', () => {
+            expect(routers.stack.length).toBe(6)
+        })
+    })
+
     it('should create a new post', async () => {
         const res = await request(app)
             .post('/api/posts')
