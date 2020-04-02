5b002c181d675f80e2cd8b38cda1d51629d98f2b
DenimCity <altidorj1@gmail.com>
2020-04-01 23:33:05 -0400

    linting

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,5 +1,6 @@
-const request = require('supertest')
-const app = require('../server')
+const request = require('supertest');
+const app = require('../server');
+
 describe('Post Endpoints', () => {
     it('should create a new post', async (done) => {
         const res = await request(app)
