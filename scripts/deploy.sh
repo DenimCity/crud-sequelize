57d24d066b729f586edd58b8c58c9d5b6a147ceb
DenimCity <altidorj1@gmail.com>
2020-04-04 07:57:06 -0400

    not test script will create TEST_DATABASE env variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@ heroku create desired-app-name-prod --buildpack heroku/nodejs --remote productio
 heroku config:set NPM_CONFIG_PRODUCTION=false YARN_PRODUCTION=false NODE_ENV=test --app desired-app-name-qa
 heroku config:set NPM_CONFIG_PRODUCTION=true YARN_PRODUCTION=true NODE_ENV=production --app desired-app-name-prod
 
-heroku addons:create heroku-postgresql --app desired-app-name-qa
+heroku addons:create heroku-postgresql --app desired-app-name-qa  --as TEST_DATABASE
 heroku addons:create heroku-postgresql --app desired-app-name-prod
