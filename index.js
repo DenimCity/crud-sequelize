2f95ae8d56e10b6663dd99f70bd4efa3e7f39990
Jean Altidor <altidorj1@gmail.com>
2020-04-04 14:16:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const CONFIG = require('./config/app');
 const server = require('./server');
 const logger = require('./utils/logger');
 
-server.listen(CONFIG.PORT, () => logger.info(`Server is running on PORT: ${CONFIG.PORT}`));
+server.listen(CONFIG.PORT, () => logger.info(`Server is OK running on PORT: ${CONFIG.PORT}`));
