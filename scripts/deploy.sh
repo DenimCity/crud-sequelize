6e08857136686a755013348c3aa580a4972d2137
DenimCity <altidorj1@gmail.com>
2020-04-04 12:38:32 -0400

    modified workflow

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
-heroku create desired-app-name-qa --buildpack heroku/nodejs --remote staging
-heroku create desired-app-name-prod --buildpack heroku/nodejs --remote production
+heroku create desired-app-name-qa --buildpack heroku/nodejs --remote qa
+heroku create desired-app-name-prod --buildpack heroku/nodejs --remote prod
 
-heroku config:set NPM_CONFIG_PRODUCTION=false YARN_PRODUCTION=false NODE_ENV=test --app desired-app-name-qa
+heroku config:set NPM_CONFIG_PRODUCTION=false YARN_PRODUCTION=false NODE_ENV=qa --app desired-app-name-qa
 heroku config:set NPM_CONFIG_PRODUCTION=true YARN_PRODUCTION=true NODE_ENV=production --app desired-app-name-prod
 
 heroku addons:create heroku-postgresql --app desired-app-name-qa  --as TEST_DATABASE
